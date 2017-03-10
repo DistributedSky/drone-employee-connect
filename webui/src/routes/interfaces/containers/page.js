d53dce65c74bf447fe1f8c24e31e9fa188055250
vol4tim <vol4tim@gmail.com>
2017-03-10 16:56:36 +0400

    getHardware start page

--- webui/src/routes/interfaces/containers/page.js
+++ webui/src/routes/interfaces/containers/page.js
@@ -1,18 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+import { getHardware } from '../../../modules/interfaces/actions';
 
-const Container = (props) => {
-  const { children, internet } = props;
-  return (<div>
-    <h1>Interfaces<div className={internet ? 'label label-success pull-right' : 'label label-danger pull-right'}>internet</div></h1>
-    <hr />
-    {children}
-  </div>);
-};
+class Container extends Component {
+  componentWillMount() {
+    if (!this.props.isHardware) {
+      this.props.getHardware();
+    }
+  }
+
+  componentWillReceiveProps(next) {
+    if (!next.isHardware) {
+      this.props.getHardware();
+    }
+  }
+
+  render() {
+    const { children, internet } = this.props;
+    return (<div>
+      <h1>Interfaces<div className={internet ? 'label label-success pull-right' : 'label label-danger pull-right'}>internet</div></h1>
+      <hr />
+      {children}
+    </div>);
+  }
+}
 
 Container.propTypes = {
   children: PropTypes.element.isRequired,
-  internet: PropTypes.bool
+  internet: PropTypes.bool,
+  isHardware: PropTypes.bool.isRequired,
+  getHardware: PropTypes.func.isRequired
 };
 
 Container.defaultProps = {
@@ -21,8 +40,17 @@ Container.defaultProps = {
 
 function mapStateToProps(state) {
   return {
-    internet: state.interfaces.hardware.internet
+    internet: state.interfaces.hardware.internet,
+    isHardware: !_.isEmpty(state.interfaces.hardware)
+  };
+}
+function mapDispatchToProps(dispatch) {
+  const actions = bindActionCreators({
+    getHardware,
+  }, dispatch);
+  return {
+    getHardware: actions.getHardware,
   };
 }
 
-export default connect(mapStateToProps)(Container);
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
