ac16abef04a94f9c74b843aec786bcee5f738710
Alexander Krupenkin <mail@akru.me>
2017-03-14 12:16:58 +0300

    Added new API

--- models.py
+++ models.py
@@ -3,44 +3,18 @@ from application import app
 
 db = SQLAlchemy(app)
 
-class WifiSettings(db.Model):
-    id       = db.Column(db.Integer, primary_key=True)
-    iface    = db.Column(db.String(20), unique=True)
-    ssid     = db.Column(db.String(120))
-    password = db.Column(db.String(120))
-
-    def __init__(self, iface, ssid, password):
-        self.iface    = iface
-        self.ssid     = ssid
-        self.password = password
-
-    def __repr__(self):
-        return '<WifiSettings %r>'.format(self.iface_id)
-
-class Link(db.Model):
-    id    = db.Column(db.Integer, primary_key=True)
-    iface = db.Column(db.String(20), unique=True)
-    short = db.Column(db.String(20), unique=True)
-
-    def __init__(self, iface, short):
-        self.iface = iface
-        self.short = short
-
-    def __repr__(self):
-        return '<Link %r %r>'.format(self.iface, self.short)
-
 class DroneInfo(db.Model):
-    id      = db.Column(db.Integer, primary_key=True)
-    iface   = db.Column(db.String(20), unique=True)
-    battery = db.Column(db.Integer)
-    signal  = db.Column(db.Integer)
-    stamp   = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    container = db.Column(db.String(20), unique=True)
+    battery   = db.Column(db.Integer)
+    signal    = db.Column(db.Integer)
+    stamp     = db.Column(db.Integer)
 
-    def __init__(self, iface, battery, signal, stamp):
-        self.iface   = iface
-        self.battery = battery
-        self.signal  = signal
-        self.stamp   = stamp
+    def __init__(self, container, battery, signal, stamp):
+        self.container = iface
+        self.battery   = battery
+        self.signal    = signal
+        self.stamp     = stamp
 
     def __repr__(self):
-        return '<Drone %r>'.format(self.iface)
+        return '<Drone %r>'.format(self.container)
--- resources.py
+++ resources.py
@@ -1,98 +1,70 @@
 from flask_restful import Resource, Api, reqparse
 from platform import machine, processor, system
 from os import system as system_call
-from netifaces import ifaddresses
-from wireless import Wireless
+from netifaces import interfaces 
 from docker import from_env
 import psutil, time
 from models import *
 
 API_PREFIX = '/api/v1'
 
-class Interfaces(Resource):
-  def get(self):
-    return {'interfaces': Wireless().interfaces()} 
-
-class Interface(Resource):
+class Containers(Resource):
     def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('ssid')
-        self.parser.add_argument('password')
+        self.docker = from_env()
 
-    def get(self, iface):
-        return {'interfaces': {
-                    iface: ifaddresses(iface)
-                }}
-
-    def post(self, iface):
-        args = self.parser.parse_args()
-        res  = Wireless(iface).connect(args['ssid'], args['password'])
-
-        if res:
-            wifi = WifiSettings.query.filter_by(iface=iface).first()
-            try:
-                wifi.ssid = args['ssid']
-                wifi.password = args['password']
-            except:
-                db.session.add(WifiSettings(iface, args['ssid'], args['password']))
-            db.session.commit()
-
-        return {'interfaces': {
-                    iface: {
-                        'ssid': args['ssid'],
-                        'password': args['password'],
-                        'connected': res
-                    }}}
+    def get(self):
+        containerIds = map(lambda x: x.short_id, self.docker.containers.list())
+        return {'containers': list(containerIds)} 
 
 class Container(Resource):
     def __init__(self):
         self.docker = from_env()
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('image')
+        self.parser.add_argument('ssid')
+        self.parser.add_argument('password')
 
-    def get(self, iface):
-        link = Link.query.filter_by(iface=iface).first()
+    def get(self, short_id):
+        c = self.docker.containers.get(short_id)
+        return {'containers': {
+                    short_id: {
+                        'status': c.status,
+                        }}}
+
+    def delete(self, short_id):
         try:
-            c = self.docker.containers.get(link.short)
-            return {'containers': {
-                    iface: {
-                        link.short: {
-                            'status': c.status,
-                        }}}}
-        except:
-            return {'error': 'Container not found'}
-
-    def post(self, iface):
-        args = self.parser.parse_args()
-        container = self.docker.containers.run('droneemployee/'+args['image'], detach=True)
-
-        link = Link.query.filter_by(iface=iface).first()
-        try:
-            c = self.docker.containers.get(link.short);
+            c = self.docker.containers.get(short_id);
             c.stop()
             c.remove()
-            link.short = container.short_id
-        except:
-            db.session.add(Link(iface, container.short_id))
-        db.session.commit()
+            return {'success': True}
+        except e:
+            return {'success': False, 'error': e}
 
+    def post(self):
+        args = self.parser.parse_args()
+        container = self.docker.containers.run('droneemployee/'+args['image'],
+                                                privileged=True,
+                                                detach=True)
         return {'containers': {
-                    iface: {
-                        container.short_id: {
-                            'status': container.status,
-                            'image': args['image']
-                        }
+                    container.short_id: {
+                        'status': container.status
                     }}}
+
+class ContainerLogs(Resource):
+    def get(self, short_id):
+        return from_env().containers.get(short_id).logs() 
 
 class Hardware(Resource):
     def get(self):
         ping_param = "-n 1" if system().lower()=="windows" else "-c 1"
+        hasInternet = system_call("ping " + ping_param + " github.com") == 0
+        wlans = filter(lambda x: x.startswith('wlan'), interfaces())
         return {'hardware':
-                    { 'processor': processor()
-                    , 'system': system()
+                    { 'system': system()
                     , 'arch': machine()
                     , 'time': time.time()
-                    , 'internet': system_call("ping " + ping_param + " github.com") == 0
+                    , 'internet': hasInternet 
+                    , 'wlans': list(wlans) 
                     , 'usage': {
                         'cpu': psutil.cpu_percent(interval=1, percpu=True),
                         'mem': psutil.virtual_memory().percent
@@ -105,24 +77,24 @@ class Drone(Resource):
         self.parser.add_argument('signal')
         self.parser.add_argument('stamp')
 
-    def get(self, iface):
-        drone = DroneInfo.query.filter_by(iface=iface).first()
+    def get(self, short_id):
+        drone = DroneInfo.query.filter_by(container=short_id).first()
         return {'drones':{
-                    iface: {
+                    short_id: {
                     'battery': drone.battery,
                     'signal': drone.signal,
                     'stamp': drone.stamp
                     }}}
 
-    def post(self, iface):
+    def post(self, short_id):
         args = self.parser.parse_args()
-        db.session.add(DroneInfo(iface, args['battery'], args['signal'], args['stamp']))
+        db.session.add(DroneInfo(short_id, args['battery'], args['signal'], args['stamp']))
         db.session.commit()
         return {'success': True}
 
 api = Api(app)
-api.add_resource(Interfaces, API_PREFIX+'/interfaces')
-api.add_resource(Interface,  API_PREFIX+'/interfaces/<iface>')
-api.add_resource(Container,  API_PREFIX+'/containers/<iface>')
-api.add_resource(Drone,      API_PREFIX+'/drones/<iface>')
+api.add_resource(Containers,    API_PREFIX+'/containers')
+api.add_resource(Container,     API_PREFIX+'/containers/<short_id>')
+api.add_resource(ContainerLogs, API_PREFIX+'/containers/<short_id>/logs')
+api.add_resource(Drone,         API_PREFIX+'/drones/<short_id>')
 api.add_resource(Hardware,   API_PREFIX+'/hardware')
