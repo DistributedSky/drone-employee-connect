c6d1a8d9a417164b293aede6847ce49f325391c8
muchosun <muchosun@gmail.com>
2017-08-31 12:10:29 +0000

    Docker image stabilization

--- wifi.py
+++ wifi.py
@@ -4,7 +4,7 @@ from docker import from_env
 
 def spawn(name, wlan, ssid, password):
     c = from_env().containers.get(name)
-    iwcall = 'iw phy phy{0} set netns {1}'.format(wlan[-1], c.attrs['State']['Pid'])
+    iwcall ='PHY="phy"`iw dev {0} info | grep wi | grep -Eo ".{{1}}$"`; iw phy $PHY set netns {1}'.format(wlan, c.attrs['State']['Pid'])
     wpacall = 'sh -c "wpa_passphrase {0} {1} > /tmp/wpa.conf"'.format(ssid, password)
     supcall = 'sudo wpa_supplicant -B -i {0} -c /tmp/wpa.conf'.format(wlan)
     print(system_call(iwcall))
@@ -12,8 +12,13 @@ def spawn(name, wlan, ssid, password):
     print(c.exec_run(supcall))
     print(c.exec_run('sudo dhcpcd {0}'.format(wlan)))
 
+import time
+time.sleep(30)
 if __name__ == '__main__':
     links = [x[:-1].split(',') for x in open('/etc/dronelinks.csv', 'r')]
 
     for name, wlan, ssid, password in links:
-        spawn(name, wlan, ssid, password)
+        try:
+            spawn(name, wlan, ssid, password)
+        except:
+            print('broken ' + name)
