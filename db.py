24e8aa99aafe6625f65664e681b149ed12bcfd39
muchosun <muchosun@gmail.com>
2017-09-13 06:08:26 +0000

    Add unique field for wlan

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ class Wlans(Base):
     __tablename__ = 'wlanstb'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    wlan = Column(String)
+    wlan = Column(String(30), nullable=False, unique=True)
     ssid = Column(String)
     password = Column(String)
 
